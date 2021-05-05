ea238d8e2a11d136ec5dfd87f967b354ec8b83d1
0n1dev <all_mind@naver.com>
2021-05-06 00:01:09 +0900

    [#5] Event 201 응답 받기

--- src/test/java/kr/spring/restapi/events/EventControllerTest.java
+++ src/test/java/kr/spring/restapi/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package kr.spring.restapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -19,11 +23,29 @@ public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Test")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 5, 5, 10, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 5, 7, 10, 0))
+                .beginEventDateTime(LocalDateTime.of(2021, 5, 12, 10, 0))
+                .endEventDateTime(LocalDateTime.of(2021,5,13,10,0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
         mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON))
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
                 .andExpect(status().isCreated());
     }
 }
