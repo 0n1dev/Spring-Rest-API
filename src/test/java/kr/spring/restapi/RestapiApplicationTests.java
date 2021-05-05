51fa82f09539b7fa44b67fb422a52c06c688a0e1
0n1dev <all_mind@naver.com>
2021-05-05 21:32:36 +0900

    Init Project

---
+++ src/test/java/kr/spring/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spring.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
